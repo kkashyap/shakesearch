b961769f971410ba0b84f4f411fefe007de16685
Kapil Kashyap <kkashyap@berkeley.edu>
2021-02-28 22:08:32 -0800

    Bring over commits from feature branch

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
 	"os"
 )
 
@@ -35,9 +36,26 @@ func main() {
 	}
 }
 
+var ListOfWorks = []string {"THE SONNETS", "ALL’S WELL THAT ENDS WELL", "THE TRAGEDY OF ANTONY AND CLEOPATRA", "AS YOU LIKE IT", "THE COMEDY OF ERRORS", "THE TRAGEDY OF CORIOLANUS", "CYMBELINE", 
+								"THE TRAGEDY OF HAMLET, PRINCE OF DENMARK", "THE FIRST PART OF KING HENRY THE FOURTH", "THE SECOND PART OF KING HENRY THE FOURTH", "THE LIFE OF KING HENRY THE FIFTH", "THE FIRST PART OF HENRY THE SIXTH", "THE SECOND PART OF KING HENRY THE SIXTH", "THE THIRD PART OF KING HENRY THE SIXTH", "KING HENRY THE EIGHTH",
+								"KING JOHN", "THE TRAGEDY OF JULIUS CAESAR", "THE TRAGEDY OF KING LEAR", "LOVE’S LABOUR’S LOST", "THE TRAGEDY OF MACBETH", "MEASURE FOR MEASURE", "MEASURE FOR MEASURE", "THE MERCHANT OF VENICE", "THE MERRY WIVES OF WINDSOR", "A MIDSUMMER NIGHT’S DREAM", "MUCH ADO ABOUT NOTHING", "THE TRAGEDY OF OTHELLO, MOOR OF VENICE",
+								"PERICLES, PRINCE OF TYRE", "KING RICHARD THE SECOND", "KING RICHARD THE THIRD", "THE TRAGEDY OF ROMEO AND JULIET", "THE TAMING OF THE SHREW", "THE TEMPEST", "THE LIFE OF TIMON OF ATHENS", "THE TRAGEDY OF TITUS ANDRONICUS", "THE HISTORY OF TROILUS AND CRESSIDA", "TWELFTH NIGHT; OR, WHAT YOU WILL",
+								"THE TWO GENTLEMEN OF VERONA", "THE TWO GENTLEMEN OF VERONA", "THE TWO NOBLE KINSMEN", "THE WINTER’S TALE", "A LOVER’S COMPLAINT", "THE PASSIONATE PILGRIM", "THE PHOENIX AND THE TURTLE", "THE RAPE OF LUCRECE", "VENUS AND ADONIS"}       
+
 type Searcher struct {
 	CompleteWorks string
 	SuffixArray   *suffixarray.Index
+	WorkIndicesMap map[string]*WorkIndices
+}
+
+type SearchResult struct {
+	ResultString string
+	WorkTitle string
+}
+
+type WorkIndices struct {
+	StartIndex int
+	EndIndex int
 }
 
 func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request) {
@@ -62,21 +80,51 @@ func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request
 	}
 }
 
-func (s *Searcher) Load(filename string) error {
+func (s *Searcher) Load(filename string) error {         
+	s.WorkIndicesMap = make(map[string]*WorkIndices)
 	dat, err := ioutil.ReadFile(filename)
 	if err != nil {
 		return fmt.Errorf("Load: %w", err)
 	}
 	s.CompleteWorks = string(dat)
 	s.SuffixArray = suffixarray.New(dat)
+	for index, work := range ListOfWorks {
+		s.WorkIndicesMap[work] = &WorkIndices{}
+		if index == 0 {
+			s.WorkIndicesMap[work].StartIndex = 0
+		} else {
+			lookupResult := s.SuffixArray.Lookup([]byte(work), 1)
+			if len(lookupResult) > 0 {
+				currIndex := lookupResult[0]
+				s.WorkIndicesMap[work].StartIndex = currIndex
+				s.WorkIndicesMap[ListOfWorks[index - 1]].EndIndex = currIndex - 1
+				if index == len(ListOfWorks) - 1 {
+					s.WorkIndicesMap[work].EndIndex = len(s.CompleteWorks)
+				}
+			}
+		}
+	}
 	return nil
 }
 
-func (s *Searcher) Search(query string) []string {
-	idxs := s.SuffixArray.Lookup([]byte(query), -1)
-	results := []string{}
+func (s *Searcher) findWorkTitle(searchIndex int) string {
+	for _, work := range ListOfWorks {
+		if searchIndex >= s.WorkIndicesMap[work].StartIndex && searchIndex < s.WorkIndicesMap[work].EndIndex {
+			return work
+		}
+	}
+	return "Unidentified Source Material"
+}
+
+func (s *Searcher) Search(query string) []SearchResult {
+	re := regexp.MustCompile("(?i)" + query)
+	idxs := s.SuffixArray.FindAllIndex(re, -1)
+	results := []SearchResult{}
 	for _, idx := range idxs {
-		results = append(results, s.CompleteWorks[idx-250:idx+250])
+		result := SearchResult{
+			ResultString: s.CompleteWorks[idx[0]-250:idx[0]+250],
+			WorkTitle: s.findWorkTitle(idx[0]) }
+		results = append(results, result)
 	}
 	return results
 }
--- static/app.js
+++ static/app.js
@@ -5,18 +5,22 @@ const Controller = {
     const data = Object.fromEntries(new FormData(form));
     const response = fetch(`/search?q=${data.query}`).then((response) => {
       response.json().then((results) => {
-        Controller.updateTable(results);
+        Controller.updateTable(results, data.query);
       });
     });
   },
 
-  updateTable: (results) => {
+  updateTable: (results, query) => {
     const table = document.getElementById("table-body");
     const rows = [];
+    let queryRegEx = new RegExp(query, 'ig');
+    let resultString = results.length == 1 ? "result" : "results";
+    rows.push(`<th>Found ${results.length} ${resultString} for ${query}</th>`);
     for (let result of results) {
-      rows.push(`<tr>${result}<tr/>`);
+      let searchResult = result.ResultString.replace(queryRegEx, function(str) {return '<b>'+str+'</b>'});
+      rows.push(`<tr>${searchResult}... <i>(Found in ${result.WorkTitle})</i><tr/><hr class="solid"></hr>`);
     }
-    table.innerHTML = rows;
+    table.innerHTML = rows.join("")
   },
 };
 
